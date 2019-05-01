4ea409f382ec7060239b92bc95a3a4a80b625ca3
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-05-01 17:02:46 +0800

    init

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button as PaperButton} from 'react-native-paper';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,6 +22,9 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <PaperButton icon="add-a-photo" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </PaperButton>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
